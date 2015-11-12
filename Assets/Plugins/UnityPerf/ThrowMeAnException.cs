8c4bfe58f5c159dd8ae846c7e0ce6b1b13089b88
h-obara <h-obara@voltage.co.jp>
2015-11-12 17:57:28 +0900

    GamePerformanceReportingを追加

---
+++ Assets/Plugins/UnityPerf/ThrowMeAnException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.CrashLog;
+using System.Collections;
+
+public class ThrowMeAnException : MonoBehaviour 
+{
+
+    void Awake()
+    {
+		CrashReporting.Init("1c0b4e3e-2eea-4f7b-9cad-39b696015edf");
+    }
+
+	public void CrashForMe()
+	{
+		throw new Exception("Button press exception");
+	}
+
+}
---
+++ Assets/Scripts/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.CrashLog;
+
+
+public class HelloController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Awake()
+	{
+		CrashReporting.Init("1c0b4e3e-2eea-4f7b-9cad-39b696015edf");
+	}
+
+	public void OnPressError()
+	{
+		throw new Exception("OnPressError(): Happen something.");
+	}
+}
